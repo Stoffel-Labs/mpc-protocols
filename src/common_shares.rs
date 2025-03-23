c8822533bee54c465bf9bb3bbd4a5ee6fe3999c3
Mikerah <mikerah@stoffelmpc.com>
2025-03-23 13:18:53 -0400

    Started RBC trait and common_shares.rs with Shamir Secret Sharing

---
+++ src/common_shares.rs
@@ -0,0 +1,20 @@
+use ark_ff:{FiniteField};
+
+struct ShamirSecretSharing {
+
+}
+
+impl Share for ShamirSecretSharing {
+    type UnderlyingSecret = FiniteField;
+    fn add() {
+
+    }
+
+    fn mul() {
+
+    }
+
+    fn reveal() {
+
+    }
+}
