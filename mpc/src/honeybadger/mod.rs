7718919c6c2f52ab6285b1cec619013d2474d7a2
GarryFCR <garryranee@gmail.com>
2025-06-25 14:42:43 +0530

    Refactor

--- mpc/src/honeybadger/mod.rs
+++ mpc/src/honeybadger/mod.rs
@@ -1,3 +1,8 @@
+/// This module contains the implementation of the Robust interpolate protocol presented in
+/// Figure 1 in the paper "HoneyBadgerMPC and AsynchroMix: Practical AsynchronousMPC and its
+/// Application to Anonymous Communication".
+pub mod robust_interpolate;
+
 /// This module contains the implementation of the Batch Reconstruction protocol presented in
 /// Figure 2 in the paper "HoneyBadgerMPC and AsynchroMix: Practical AsynchronousMPC and its
 /// Application to Anonymous Communication".
@@ -7,6 +12,3 @@ pub mod batch_recon;
 /// Figure 3 in the paper "HoneyBadgerMPC and AsynchroMix: Practical AsynchronousMPC and its
 /// Application to Anonymous Communication".
 pub mod ran_dou_sha;
-pub mod robust_interpolate;
-
-pub struct HoneyBadgerMPC;
