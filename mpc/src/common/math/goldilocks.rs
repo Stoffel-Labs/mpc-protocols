c2d4a6bf1eddc3f4afef2abff89eede394f7b633
hdvanegasm <hdvanegasm@gmail.com>
2026-01-09 11:37:13 -0500

    fix(goldilocks): Implementation of the correct trait.

--- mpc/src/common/math/goldilocks.rs
+++ mpc/src/common/math/goldilocks.rs
@@ -1,8 +1,13 @@
-use ark_ff::{Fp64, MontConfig};
+use ark_ff::{Fp64, MontBackend, MontConfig};
 
+/// Configuration for the Goldilocks field.
 #[derive(MontConfig)]
 #[modulus = "18446744069414584321"]
 #[generator = "7"]
 pub struct GoldilocksMontConfig;
 
-pub type GoldilocksField = Fp64<GoldilocksMontConfig>;
+/// Number of limbs of 64 bits used by the Goldilocks field.
+const NUM_LIMBS: usize = 1;
+
+/// Goldilocks field.
+pub type GoldilocksField = Fp64<MontBackend<GoldilocksMontConfig, NUM_LIMBS>>;
